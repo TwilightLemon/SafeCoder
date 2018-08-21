88f418a30ff8981b04802ebb767fbb3b8146ab16
TwilightLemon <cz241126@live.com>
2018-08-21 17:06:17 +0800

    添加项目文件。

---
+++ SafeCoder/EPClass.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeCoder
+{
+    public class EPChangeEventArgs : EventArgs
+
+    {
+        private double EPe;
+        public EPChangeEventArgs(double message)
+        {
+            this.EPe = message;
+        }
+        public double EpsValue
+        {
+            get { return EPe; }
+        }
+    }
+    public class EPClass
+    {
+        public delegate void EPeChangedHandle(object sender, EPChangeEventArgs e);
+        public event EPeChangedHandle EPValueChanged;
+        public async Task EncryptAsync(string inFile, string outFile, string encryptKey)
+        {
+            FileInfo f = new FileInfo(inFile);
+            string filename = f.Name.Substring(0, f.Name.IndexOf('.'));
+            string dic = f.DirectoryName + "\\" + filename;
+            Directory.CreateDirectory(dic);
+            File.WriteAllText(dic + "\\md5", MD5File(inFile));
+            byte[] rgb = { 0x14, 0x28, 0x23, 0x98, 0x86, 0xCD, 0xDF, 0xEF };
+            byte[] rgbKeys = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+            FileStream inFs = new FileStream(inFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream outFs = new FileStream(dic + "\\data", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            outFs.SetLength(0);
+            byte[] byteIn = new byte[1024];
+            long readLen = 0;
+            long totalLen = inFs.Length;
+            int everylen = 0;
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(outFs, des.CreateEncryptor(rgb, rgbKeys), CryptoStreamMode.Write);
+            while (readLen < totalLen)
+            {
+                everylen = await inFs.ReadAsync(byteIn, 0, 1024);
+                await encStream.WriteAsync(byteIn, 0, everylen);
+                readLen = readLen + everylen;
+                double fo = (double)readLen / (double)totalLen;
+                EPValueChanged?.Invoke(this, new EPChangeEventArgs(fo));
+            }
+            encStream.Close();
+            inFs.Close();
+            outFs.Close();
+            ZipFile.CreateFromDirectory(dic, outFile+".sc");
+            new DirectoryInfo(dic).Delete(true);
+        }
+        public static string MD5File(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            int bufferSize = 1048576;
+            byte[] buff = new byte[bufferSize];
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            md5.Initialize();
+            long offset = 0;
+            while (offset < fs.Length)
+            {
+                long readSize = bufferSize;
+                if (offset + readSize > fs.Length) readSize = fs.Length - offset;
+                fs.Read(buff, 0, Convert.ToInt32(readSize));
+                if (offset + readSize < fs.Length) md5.TransformBlock(buff, 0, Convert.ToInt32(readSize), buff, 0);
+                else md5.TransformFinalBlock(buff, 0, Convert.ToInt32(readSize));
+                offset += bufferSize;
+            }
+            fs.Close();
+            byte[] result = md5.Hash;
+            md5.Clear();
+
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < result.Length; i++)
+                sb.Append(result[i].ToString("X2"));
+
+            return sb.ToString();
+        }
+        public async Task DecryptAsync(string inFile, string outFile, string encryptKey)
+        {
+            FileInfo f = new FileInfo(inFile);
+            string dic = f.DirectoryName + "\\" + f.Name.Substring(0, f.Name.IndexOf('.'));
+            Directory.CreateDirectory(dic);
+            ZipFile.ExtractToDirectory(inFile, dic);
+            string filename = dic + "\\data";
+            string md5 = File.ReadAllText(dic + "\\md5");
+            byte[] rgb = { 0x14, 0x28, 0x23, 0x98, 0x86, 0xCD, 0xDF, 0xEF };
+            byte[] rgbKeys = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+            FileStream inFs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream outFs = new FileStream(outFile + ".data", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            outFs.SetLength(0);
+            byte[] byteIn = new byte[1024];
+            long readLen = 0;
+            long totalLen = inFs.Length;
+            int everylen = 0;
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(outFs, des.CreateDecryptor(rgb, rgbKeys), CryptoStreamMode.Write);
+            while (readLen < totalLen)
+            {
+                everylen = await inFs.ReadAsync(byteIn, 0, 1024);
+                await encStream.WriteAsync(byteIn, 0, everylen);
+                readLen = readLen + everylen;
+                double fo = (double)readLen / (double)totalLen;
+                EPValueChanged?.Invoke(this, new EPChangeEventArgs(fo));
+            }
+            encStream.Close();
+            inFs.Close();
+            outFs.Close();
+            if (MD5File(outFile + ".data") == md5) {
+                File.Copy(outFile + ".data", outFile,true);
+                File.Delete(outFile + ".data");
+                new DirectoryInfo(dic).Delete(true);
+            }
+        }
+    }
+}
---
+++ SafeCoder/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace SafeCoder
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cs.EPValueChanged += Cs_EPValueChanged;
+            var c = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(0.3));
+            this.BeginAnimation(OpacityProperty, c);
+        }
+
+        private void Cs_EPValueChanged(object sender, EPChangeEventArgs e)
+        {
+            pro.Value = e.EpsValue;
+            if (e.EpsValue == 1)
+                tbf.Text = "已完成";
+        }
+
+        private void CLOSE_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var c = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.3));
+            c.Completed += delegate { Environment.Exit(0); };
+            this.BeginAnimation(OpacityProperty, c);
+        }
+
+        private void window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+        string path = "";
+        string vpath = "";
+        private void window_Drop(object sender, DragEventArgs e)
+        {
+            path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            tbf.Text = "已选择文件:" + path;
+            tbf.ToolTip = tbf.Text;
+            ed= TxtFileEncoder.GetEncoding(path);
+            FileInfo f = new FileInfo(path);
+            vpath = path;
+            bc.Text = "保存到:"+vpath;
+            bc.ToolTip = bc.Text;
+        }
+        Encoding ed = Encoding.Default ;
+        EPClass cs = new EPClass();
+        private async void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbf.Text = "加密中...";
+            if (path != "")
+                await cs.EncryptAsync(path, vpath, MD5.EncryptToMD5string(psw.Password));
+            else tbf.Text = "请选择加密文件";
+        }
+
+        private async void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            tbf.Text = "解密中...";
+            if (path != "")
+            {
+                if (!path.Contains(".sc")) path += ".sc";
+                await cs.DecryptAsync(path, vpath, MD5.EncryptToMD5string(psw.Password));
+            }
+            else tbf.Text = "请选择解密文件";
+        }
+
+        private void bc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.SaveFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                vpath= openFileDialog.FileName;
+                bc.Text = "保存到:"+vpath;
+                bc.ToolTip= "保存到:" + vpath;
+            }
+        }
+    }
+    public class MD5
+    {
+        public static byte[] EncryptToMD5(string str)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] str1 = System.Text.Encoding.UTF8.GetBytes(str);
+            byte[] str2 = md5.ComputeHash(str1, 0, str1.Length);
+            md5.Clear();
+            (md5 as IDisposable).Dispose();
+            return str2;
+        }
+        public static string EncryptToMD5string(string str)
+        {
+            byte[] bytHash = EncryptToMD5(str);
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+    }
+}
