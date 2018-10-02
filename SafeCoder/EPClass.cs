267d90560a4e9138e108b4a8ad762aa16f1c5e9f
Twilight <cz241126@live.com>
2018-10-02 14:44:39 +0800

    #修复一些Bug 支持取消

--- SafeCoder/EPClass.cs
+++ SafeCoder/EPClass.cs
@@ -10,20 +10,13 @@ using System.Threading.Tasks;
 namespace SafeCoder
 {
     public class EPChangeEventArgs : EventArgs
-
     {
-        private double EPe;
-        public EPChangeEventArgs(double message)
-        {
-            this.EPe = message;
-        }
-        public double EpsValue
-        {
-            get { return EPe; }
-        }
+        public EPChangeEventArgs(double message) => EpsValue = message;
+        public double EpsValue { get; }
     }
     public class EPClass
     {
+        public static bool IsStop = false;
         public delegate void EPeChangedHandle(object sender, EPChangeEventArgs e);
         public event EPeChangedHandle EPValueChanged;
         public async Task EncryptAsync(string inFile, string outFile, string encryptKey)
@@ -32,7 +25,8 @@ namespace SafeCoder
             string filename = f.Name.Substring(0, f.Name.IndexOf('.'));
             string dic = f.DirectoryName + "\\" + filename;
             Directory.CreateDirectory(dic);
-            File.WriteAllText(dic + "\\md5", MD5File(inFile));
+            File.WriteAllText(dic + "\\md5",await MD5File(inFile));
+            if (IsStop) return;
             File.WriteAllText(dic + "\\psw", MD5.EncryptToMD5string(MD5.EncryptToMD5string(encryptKey)));
             byte[] rgb = { 0x14, 0x28, 0x23, 0x98, 0x86, 0xCD, 0xDF, 0xEF };
             byte[] rgbKeys = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
@@ -47,6 +41,7 @@ namespace SafeCoder
             CryptoStream encStream = new CryptoStream(outFs, des.CreateEncryptor(rgb, rgbKeys), CryptoStreamMode.Write);
             while (readLen < totalLen)
             {
+                if (IsStop) break;
                 everylen = await inFs.ReadAsync(byteIn, 0, 1024);
                 await encStream.WriteAsync(byteIn, 0, everylen);
                 readLen = readLen + everylen;
@@ -56,10 +51,13 @@ namespace SafeCoder
             encStream.Close();
             inFs.Close();
             outFs.Close();
+            if (IsStop) { new DirectoryInfo(dic).Delete(true); return; }
+            if (File.Exists(outFile + ".sc"))
+                File.Delete(outFile + ".sc");
             ZipFile.CreateFromDirectory(dic, outFile+".sc");
             new DirectoryInfo(dic).Delete(true);
         }
-        public static string MD5File(string filePath)
+        public async static Task<string> MD5File(string filePath)
         {
             FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
             int bufferSize = 1048576;
@@ -69,14 +67,17 @@ namespace SafeCoder
             long offset = 0;
             while (offset < fs.Length)
             {
+                if (IsStop)
+                    break;
                 long readSize = bufferSize;
                 if (offset + readSize > fs.Length) readSize = fs.Length - offset;
-                fs.Read(buff, 0, Convert.ToInt32(readSize));
+                await fs.ReadAsync(buff, 0, Convert.ToInt32(readSize));
                 if (offset + readSize < fs.Length) md5.TransformBlock(buff, 0, Convert.ToInt32(readSize), buff, 0);
                 else md5.TransformFinalBlock(buff, 0, Convert.ToInt32(readSize));
                 offset += bufferSize;
             }
             fs.Close();
+            if (IsStop) return "STOP";
             byte[] result = md5.Hash;
             md5.Clear();
 
@@ -86,7 +87,27 @@ namespace SafeCoder
 
             return sb.ToString();
         }
-        public async Task<bool> DecryptAsync(string inFile, string outFile, string encryptKey)
+        public async static Task<bool> CopyFileAsync(string soucrePath, string targetPath)
+        {
+            try
+            {
+                using (FileStream fsRead = new FileStream(soucrePath, FileMode.Open, FileAccess.Read))
+                    using (FileStream fsWrite = new FileStream(targetPath, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        byte[] buffer = new byte[1024 * 1024 * 2];
+                        while (true)
+                        {
+                            int n = await fsRead.ReadAsync(buffer, 0, buffer.Count());
+                            if (n == 0)
+                                break;
+                            await fsWrite.WriteAsync(buffer, 0, n);
+                        }
+                    }
+                return true;
+            }
+            catch { return false; }
+        }
+        public async Task<int> DecryptAsync(string inFile, string outFile, string encryptKey)
         {
             FileInfo f = new FileInfo(inFile);
             string dic = f.DirectoryName + "\\" + f.Name.Substring(0, f.Name.IndexOf('.'));
@@ -111,6 +132,7 @@ namespace SafeCoder
                 CryptoStream encStream = new CryptoStream(outFs, des.CreateDecryptor(rgb, rgbKeys), CryptoStreamMode.Write);
                 while (readLen < totalLen)
                 {
+                    if (IsStop) break;
                     everylen = await inFs.ReadAsync(byteIn, 0, 1024);
                     await encStream.WriteAsync(byteIn, 0, everylen);
                     readLen = readLen + everylen;
@@ -120,15 +142,17 @@ namespace SafeCoder
                 encStream.Close();
                 inFs.Close();
                 outFs.Close();
-                if (MD5File(outFile + ".data") == md5)
+                if (IsStop) { new DirectoryInfo(dic).Delete(true); return 3; }
+                if (await MD5File(outFile + ".data") == md5)
                 {
-                    File.Copy(outFile + ".data", outFile, true);
+                    await CopyFileAsync(outFile + ".data", outFile);
                     File.Delete(outFile + ".data");
                     new DirectoryInfo(dic).Delete(true);
+                    return 0;
                 }
-                return true;
+                else return 1;        
             }
-            else { new DirectoryInfo(dic).Delete(true); return false; }
+            else { new DirectoryInfo(dic).Delete(true); return 2; }
         }
     }
 }
--- SafeCoder/MainWindow.xaml.cs
+++ SafeCoder/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
 
@@ -23,9 +24,15 @@ namespace SafeCoder
 
         private void Cs_EPValueChanged(object sender, EPChangeEventArgs e)
         {
-            pro.Value = e.EpsValue;
-            if (e.EpsValue == 1)
-                tbf.Text = "已完成";
+            if (!EPClass.IsStop)
+            {
+                if (EnorDe == 1)
+                    title.Text = $"ヾ(•ω•`)o 加密中... ({(int)(e.EpsValue * 100)}%)";
+                else title.Text = $"ヾ(•ω•`)o 解密中... ({(int)(e.EpsValue * 100)}%)";
+                pro.Value = e.EpsValue;
+                if (e.EpsValue == 1)
+                { tbf.Text = "完成啦 o((>ω< ))o"; title.Text = "SafeCoder"; }
+            }
         }
 
         private void CLOSE_MouseDown(object sender, MouseButtonEventArgs e)
@@ -34,7 +41,19 @@ namespace SafeCoder
             c.Completed += delegate { Environment.Exit(0); };
             this.BeginAnimation(OpacityProperty, c);
         }
-
+        private void enPageEx() {
+            title.SetBinding(TextBlock.TextProperty, "tbf");
+            StartPage.Visibility = Visibility.Collapsed;
+            enPage.Visibility = Visibility.Visible;
+            (Resources["Begin"] as Storyboard).Begin();
+        }
+        private void StartPageEx()
+        {
+            title.Text = "SafeCoder";
+            StartPage.Visibility = Visibility.Visible;
+            enPage.Visibility = Visibility.Collapsed;
+            (Resources["Begin"] as Storyboard).Stop();
+        }
         private void window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -57,23 +76,34 @@ namespace SafeCoder
         }
         Encoding ed = Encoding.Default ;
         EPClass cs = new EPClass();
+        int EnorDe = 0;
         private async void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            tbf.Text = "加密中...";
+            EPClass.IsStop = false;
+            enPageEx();
+            EnorDe = 1;
+            tbf.Text = "序列化文件中...ヾ(≧▽≦*)o";
             if (path != "")
                 await cs.EncryptAsync(path, vpath, MD5.EncryptToMD5string(psw.Password));
-            else tbf.Text = "请选择加密文件";
+            else tbf.Text = "请选择加密文件 (￣▽￣)\"";
+            StartPageEx();
         }
 
         private async void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
         {
-            tbf.Text = "解密中...";
             if (path != "")
             {
-                bool can =await cs.DecryptAsync(path, vpath, MD5.EncryptToMD5string(psw.Password));
-                if (!can) tbf.Text = "密码错误";
+                enPageEx();
+                EPClass.IsStop = false;
+                EnorDe = 0;
+                tbf.Text = "解密中... ヾ(≧▽≦*)o";
+                int result =await cs.DecryptAsync(path, vpath, MD5.EncryptToMD5string(psw.Password));
+                if (result == 1)
+                    tbf.Text = "文件损坏惹 (＃°Д°)";
+                if (result==2) tbf.Text = "密码错误哦 (＃°Д°)";
+                StartPageEx();
             }
-            else tbf.Text = "请选择解密文件";
+            else tbf.Text = "请选择解密文件  (￣▽￣)\"";
         }
 
         private void bc_MouseDown(object sender, MouseButtonEventArgs e)
@@ -87,13 +117,21 @@ namespace SafeCoder
                 bc.ToolTip= "保存到:" + vpath;
             }
         }
+
+        private void Border_MouseDown_2(object sender, MouseButtonEventArgs e)
+        {
+            EPClass.IsStop = true;
+            tbf.Text = "取消啦 (/▽＼)";
+            pro.Value = 0;
+            StartPageEx();
+        }
     }
     public class MD5
     {
         public static byte[] EncryptToMD5(string str)
         {
             MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            byte[] str1 = System.Text.Encoding.UTF8.GetBytes(str);
+            byte[] str1 = Encoding.UTF8.GetBytes(str);
             byte[] str2 = md5.ComputeHash(str1, 0, str1.Length);
             md5.Clear();
             (md5 as IDisposable).Dispose();
